b96ae495efb7815c7d0b07bfd83df2af8a0baed3
MarynaFundament <marina.fundament@ukr.net>
2023-03-12 14:05:14 +0200

    first commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,48 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const divInsert = document.querySelector(`.gallery`)
+// console.log(divInsert)
+const cardsMarkup = createDivEl(galleryItems);
+divInsert.insertAdjacentHTML(`beforeend`, cardsMarkup )
+
+function createDivEl(items){
+    const markup = galleryItems.map(({preview,original,description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" 
+       src="${preview}"
+       alt="${description}" />
+    </a>`;
+    });
+    return markup.join('');
+  }
+
+
+  divInsert.addEventListener(`click`, handleClickElement)
+
+  
+  function handleClickElement(event) {
+    event.preventDefault();
+  
+    const targetEl = event.target;
+    const targetPicture = targetEl.classList.contains('gallery__image');
+    if (!targetPicture) {
+      return;
+    }
+  
+    
+const lightbox = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  }
+  
